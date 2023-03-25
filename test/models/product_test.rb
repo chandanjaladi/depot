16c41f60d94fc52ae31ffa6061678ada1d4bd293
cj00216 <cj00216@cs6242-vm15.uwg.westga.edu>
2023-03-25 18:46:39 -0400

    added sqlite db files

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -14,7 +14,8 @@ class ProductTest < ActiveSupport::TestCase
     product = Product.new(title: "My Book Title",description:"yyy",image_url:"zzz.jpg")
     product.price = -1
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],product.errors[:price]
+    assert_equal ["must be greater than or equal to
+      0.01"],product.errors[:price]
     product.price = 0
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],product.errors[:price]
