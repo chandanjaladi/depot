c0324bce796ce3091512d905d285e96ca389acf3
cj00216 <cj00216@cs6242-vm15.uwg.westga.edu>
2023-03-25 23:25:02 -0400

    Migrated again for the product items in the cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_26_031254) do
+ActiveRecord::Schema.define(version: 2023_03_26_032140) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
