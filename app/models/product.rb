1e80205a60e372eab74bcf38a6f70968bbdf25c3
cj00216 <cj00216@cs6242-vm15.uwg.westga.edu>
2023-03-25 22:59:14 -0400

    Done with chapter 9, add button is added to the page and when clicked it is redirecting to the page displaying that the item is added to the cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,15 @@ class Product < ApplicationRecord
   validates :title, uniqueness:true
   validates :image_url, allow_blank:true, format:{with: %r{\.(gif|jpg|png)\z}i, message: 'Must be a URL for GIF,JPF or PNG Image'}
 
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
